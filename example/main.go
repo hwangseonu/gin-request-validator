aa14b1b1bf4833812311ccd4ab47e13ed6949463
hwangseonu <hwangseonu12@naver.com>
2019-03-13 11:46:20 +0900

    feat: Add example code

---
+++ example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/hwangseonu/gin-request-validator"
+	"log"
+	"net/http"
+)
+
+type Data struct {
+	Email string `json:"email" validate:"email"`
+	Age   int    `json:"age" validate:"min=1 max=7"`
+}
+
+func main() {
+	r := gin.Default()
+
+	r.Use(gin_validator.JsonRequiredMiddleware(Data{}))
+
+	r.POST("/", func(c *gin.Context) {
+		req := gin_validator.GetJsonData(c).(Data)
+		c.JSON(http.StatusOK, req)
+	})
+
+	log.Fatal(r.Run(":5000"))
+}
